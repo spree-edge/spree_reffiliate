d1a95d065583adef1ed385c398a578fa0f5aa163
vishal <vishal@bluebash.co>
2025-07-14 18:54:42 +0530

    Fix: Fixed the redirection issue

--- app/controllers/spree/reffiliate_controller.rb
+++ app/controllers/spree/reffiliate_controller.rb
@@ -1,9 +1,9 @@
 module Spree
-  class ReffiliateController < ApplicationController
+  class ReffiliateController < Spree::BaseController
     def referral
       flash[:success] = Spree.t(:referral_code_applied)
       session[:referral] = params[:code]
-      redirect_to root_path
+      redirect_to request.base_url
     end
 
     def affiliate
@@ -11,7 +11,7 @@ module Spree
       affiliate = Spree::Affiliate.find_by(path: params[:path])
       flash[:success] = Spree.t(:affiliate_code_applied) unless affiliate.nil?
       if affiliate.nil? or affiliate.partial.blank? or !partial_exists affiliate.partial
-        redirect_to(root_path)
+        redirect_to(request.base_url)
       elsif partial_exists affiliate.partial
         render "spree/affiliates/#{affiliate.partial}", layout: affiliate.get_layout
       end
